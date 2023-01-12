ccb10c82900de34a40f01d5d376ea8e60f2bac02
sbellina <shawn.bellina@gmail.com>
2023-01-12 03:33:28 -0300

    s

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+// 
+// https://www.vuemastery.com/blog/vue-router-a-tutorial-for-vue-3/
+// 
+<script setup>
+
+</script>
+
+<template>
+<div>
+        <router-view/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
