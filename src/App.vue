c6a58ec753e13dc1a05a8103ee482992d278267e
sbellina <shawn.bellina@gmail.com>
2023-01-13 22:57:35 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 
     <div class='page'>
         <div class='header'>
-            <div>
+            <div class="header_pad">
                 <img alt="Vue logo" class="responsive" src="./assets/header.svg" height="100" />
             </div>
         </div>
@@ -92,12 +92,16 @@ onMounted(() => {
 
 
 <style scoped>
+.header_pad{
+    padding: 10px;
+}
 .title {
   font-size: 3rem;
   font-weight: 700;
   letter-spacing: -.1rem;
   line-height: 2.5rem;
   padding-bottom: 2.5rem;;
+  color: white;
 
 }
 .container {
